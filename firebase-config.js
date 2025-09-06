2c07433cc2190a95c2c35f3ebbf94a8b0bebfa3f
esmithore <orerodriguezesmithsergio@gmail.com>
2025-09-06 01:47:30 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,24 @@
-// Firebase Configuration
-const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_AUTH_DOMAIN",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_STORAGE_BUCKET",
-    messagingSenderId: "TU_MESSAGING_SENDER_ID",
-    appId: "TU_APP_ID",
-    measurementId: "TU_MEASUREMENT_ID"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Inicializar Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBx5NiszA3HqLg7NOMh10fmWU8Mct0pSCM",
+    authDomain: "mi-web-metodoore.firebaseapp.com",
+    projectId: "mi-web-metodoore",
+    storageBucket: "mi-web-metodoore.firebasestorage.app",
+    messagingSenderId: "620792207915",
+    appId: "1:620792207915:web:20451d3717cb69f495e86e",
+    measurementId: "G-430FWTPKK8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
